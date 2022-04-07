690c5ac6d741e8227b916e606fa53594460684ab
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-07 05:13:26 -0700

    Final fix 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * Main: main function initialized
+ * Main: main function initialized with void parameter
  * Return: 0
-*/
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
