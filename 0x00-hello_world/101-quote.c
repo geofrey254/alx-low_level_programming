c65e5ed3b5311b66f4c78efc21d6490de2c0c44f
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-07 08:03:07 -0700

    added unistd lib

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main function block
  * Return: 1
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
