8edeae122cf7102764286921c4b4fdcc67e29e53
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-07 05:31:28 -0700

    5-printf.c commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main function initialized
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pice of art");
+	return (0);
+}
