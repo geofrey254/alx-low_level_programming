ab9a039b6c59f5264a114e5a3fbcc5118b27a94c
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-08 09:26:23 -0700

    Alpha reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function declaration
+ * Descriuption: print alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'z';
+
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}	
