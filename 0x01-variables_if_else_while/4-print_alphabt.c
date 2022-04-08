e3d533f2695560681db4a1d861d17e1597403b03
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-08 08:33:01 -0700

    number 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function declaration
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		if (alph != 'e' && alph !='q')
+			putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
