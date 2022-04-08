0dcd69d56fba2c11911c387a5d43f31d8625c8f6
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-08 06:20:37 -0700

    remove trailing whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,20 +5,18 @@
  * main - main function declaration
  * Return: 0
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	/*if statement to check for number condition*/
 	if (n > 0)	
 		printf("%u is positive\n", n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%u is zero\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 
 	return (0);
