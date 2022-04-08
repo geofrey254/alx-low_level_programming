50e5dac852b20819c9b52c482d323f95842a8c33
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-08 06:18:40 -0700

    remove braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	/*if statement to check for number condition*/
-	if (n > 0)
-	{
+	if (n > 0)	
 		printf("%u is positive\n", n);
-	}
-	else if(n == 0) {
+	else if(n == 0)
 		printf("%u is zero\n", n);
-	}
-	else if(n < 0){
+	else if(n < 0)
 		printf("%d is negative\n", n);
-	}
 
 	return (0);
 }
