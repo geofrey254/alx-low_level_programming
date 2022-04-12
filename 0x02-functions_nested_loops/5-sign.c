36c8ed5e9139be2c1071e1f7ef0b73a8e77adc7a
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-12 14:29:01 -0700

    5 signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,12 +5,13 @@
  */
 int print_sign(int n)
 {
-	int n = 0;
-
 	if (n > 0)
-		return (1);
+		_putchar("+");
+		return (n);
 	else if (n == 0)
+		_putchar("0");
 		return (0);
 	else if(n < 0)
-		return (-1);
+		_putchar("-");
+		return (-n);
 }
