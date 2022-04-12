be7ab2163ff4c58eb1d9dbd39d3465d261cbbc81
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-12 14:16:57 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - Funtion declaration to print sign of a number
+ * Return:0
+ */
+int print_sign(int n)
+{
+	int nom = 0;
+
+	if (nom > 0)
+		return (1);
+	else if (nom == 0)
+		return (0);
+	else (nom < 0)
+		return (-1);
+}
