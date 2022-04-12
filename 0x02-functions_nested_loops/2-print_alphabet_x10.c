54999aedf643125ed225f3ac1c30de8da882f4fb
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-12 13:21:19 -0700

    Print 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,11 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char alph;
+	char c;
 	
 	while (count++ <= 9){
-		for (alph = 'a'; alph <= 'z'; alph++)
-			_putchar(alph);
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
 	_putchar('\n');
 	}	
 }
