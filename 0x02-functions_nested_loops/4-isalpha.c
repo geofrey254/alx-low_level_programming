ca6e3027c4ad981bb79b8504429b27a4081ea59e
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-12 13:58:44 -0700

    Is alpha fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,10 +8,10 @@
  */
 int _isalpha(int c)
 {
-        if (c >= 'a' && c <= 'z')
-                return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
         else if (c >= 'A' && c<= 'Z')
-                return (1);
+		return (1);
 	else
 		return (0);
 }
