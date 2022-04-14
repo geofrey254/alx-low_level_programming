f046d2343fe2c1cda65501f1216a58796449a014
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-14 11:09:57 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,20 @@
  * Return:void
  */
 
-void more_numbers(void);
+void more_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i < 10; i++)
 	{
 		int j;
 
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > '9')
-				_putchar(j/10);
+			if (j > 9)
+				_putchar((j/10) + '0');
 
-			_putchar(j%10);
+			_putchar((j%10) + '0');
 		}
 		_putchar('\n');
 	}
