ec3c476afd24c32d815db71c61b4a124f4e8be56
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-21 12:46:49 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,10 +9,16 @@
 
 void rev_string(char *s)
 {
-	int i = strlen(s) - 1;
+	int len = 0, index = 0;
+	char tmp;
 
-	while (i >= 0)
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		i--;
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
 }
