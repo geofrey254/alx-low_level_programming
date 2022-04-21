f0ae08c9734d0f0498c6814e9fab8a6c0ca3520e
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-21 11:43:22 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - starting point
+ * @str: variable declaration
+ * Return: always void
+ */
+
+void print_rev(char *s)
+{
+	*strrev(char *s);
+}
