aa3f82338623771368ebfa034c27586e1b58a445
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-21 12:26:33 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - starting point
+ * @s: variable declaration
+ * Return: always void
+ */
+
+void rev_string(char *s)
+{
+	int i = strlen(s) - 1;
+
+        while (i >= 0)
+        {
+		i--;
+        }
+}
