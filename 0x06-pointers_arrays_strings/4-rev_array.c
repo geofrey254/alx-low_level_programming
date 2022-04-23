cf85da28d89e674b5fef802594cfe89084f7450a
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-23 08:28:53 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_arrray - starting point
+ * @a: array variable name
+ * @n: int varaiable name
+ * Return: always 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = n - 1;
+
+	while (i >= 0)
+	{
+		i--;
+	}
+}
