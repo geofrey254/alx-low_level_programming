8aa621171799f0b09540de7abf4a30f4abf54db5
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-23 04:29:14 -0700

    first commit strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - starting point
+ * @dest: string var declared
+ * @src: string two var declared
+ * @n: integer car declared
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	
+	while (dest[i] != 0)
+		i++;
+
+	j = 0;
+
+	while (src[j] != 0 && <= n)
+	{
+		src[j] = dest[i];
+		j++;
+		i++;
+	}
+
+	return (dest);
+}
