1ec85b592c0d6f3f01f4709b71e08a964fb8b2fc
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-23 02:35:54 -0700

    Concatenate strintgs

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - function initialization
+ * @dest: first string variable declared
+ * @src: second variable declared
+ * Return: always char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int str;
+
+	str = src + dest;
+
+	return str;
+}
