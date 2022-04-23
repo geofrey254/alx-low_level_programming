a6b44b5ddc97fe7b74af3f087ac4d3f7e229f2c8
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-23 05:27:08 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - starting point
+ * @s1: s1 declared
+ * @s2: s2 declared
+ * Return: 0, <0, >0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+		return (0);
+}
