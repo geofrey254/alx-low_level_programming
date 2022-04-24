2361046891a3d943cbacec6014b691ab1231b4a4
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-24 04:46:55 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - starting point
+ * Return: always char
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char n[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char m[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 54; j++)
+		{
+			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+			&& s[i] == n[j])
+			{
+				s[i] = m[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
