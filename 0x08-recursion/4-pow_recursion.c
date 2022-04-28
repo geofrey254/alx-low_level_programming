89b075007707ad392773daa5d1fe3037e55c8009
geofrey254 <devongeoffreymaina@gmail.com>
2022-04-28 06:39:13 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - starting point
+ * @x: int x
+ * @y: int y
+ * Return: always int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * (_pow_recursion(x, y-1));
+}
