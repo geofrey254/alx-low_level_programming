d955c8270501274d32d0ee545da70fd4ed135d37
geofrey254 <devongeoffreymaina@gmail.com>
2022-05-04 00:21:57 -0700

    first commit args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - starting point
+ * @argc: counter
+ * @argv: array holding c
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(" \n Name of my Program %s \t", argv[0]);
+}
