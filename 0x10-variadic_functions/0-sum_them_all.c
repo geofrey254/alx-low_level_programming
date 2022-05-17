5171aa9fda37f88620ecd34378ab13f089d9b315
geofrey254 <devongeoffreymaina@gmail.com>
2022-05-17 05:08:05 -0700

    fix sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n != 0)
 		for (unsigned int i = 0; i < n; i++)
-			tot_sum += va_arg(pntr, unsigned int);
+			tot_sum += va_arg(pntr, int);
 	else
 		return (0);
 	va_end(pntr);
