c4287c2ecad314459732204dd5d02ae237a9b0d7
geofrey254 <devongeoffreymaina@gmail.com>
2022-05-17 04:29:32 -0700

    sum of parameters using variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - the function prototype
+ * @n: unsigned integer n declaration
+ * Return: total sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int tot_sum = 0;
+
+	va_list pntr;
+	va_start(pntr, n);
+
+	for (int i = 0; i < n; i++)
+		tot_sum += va_arg(pntr, int);
+
+	va_end(pntr);
+
+	return (tot_sum);
+}
