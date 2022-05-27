ff9a00aee064354daba0e21ac73227982c4964af
geofrey254 <devongeoffreymaina@gmail.com>
2022-05-27 07:46:36 -0700

    Change lists.h file with struct description

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,6 +9,9 @@
  * struct listint_s - singly linked list
  * @n: integer variable
  * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ *
  */
 
 typedef struct listint_s
