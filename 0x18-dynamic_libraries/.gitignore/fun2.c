d8a6d372ebfe40c6b4379b881595a48806413daf
geofrey254 <devongeoffreymaina@gmail.com>
2022-07-11 12:22:56 -0700

    gitignore

---
+++ 0x18-dynamic_libraries/.gitignore/fun2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - fucntion to get lowercase
+ * @s: variable c declaration
+ * @b: variable b
+ * @n: variable n
+ * Return: returns char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
+
+/**
+ * _memcpy - fucntion to get lowercase
+ * @dest: variable c declaration
+ * @src: variable b
+ * @n: variable n
+ * Return: returns char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
+
+/**
+ * _strchr - fucntion to get lowercase
+ * @s: variable s declaration
+ * @c: variable c
+ * Return: returns char
+ */
+char *_strchr(char *s, char c)
+{
+}
+
+/**
+ * _strspn - fucntion to get lowercase
+ * @s: variable c declaration
+ * @accept: variable b
+ * Return: returns char
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+}
---
+++ 0x18-dynamic_libraries/.gitignore/oth.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _stpbrk - fucntion to get lowercase
+ * @s: variable c declaration
+ * @accept: variable accept
+ * Return: returns char
+ */
+char *_stpbrk(char *s, char *accept)
+{
+}
+
+/**
+ * _strstr - fucntion to get lowercase
+ * @haystack: variable haystack
+ * @needle: variable needle
+ * Return: returns int
+ */
+char *_strstr(char *haystack, char *needle)
+{
+}
